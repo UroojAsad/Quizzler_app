6d78fd740f08e1bbab8e8910ead7d0775b977e0d
UroojAsad <uroojshehzadi82@gmail.com>
2024-07-20 14:18:51 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() {
+  runApp( Quizzler());
+}
+
+class Quizzler extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey,
+        body: SafeArea(child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+          child: quizpage(),
+        )
+
+        ),
+      ),
+    );
+  }
+}
+class quizpage extends StatefulWidget {
+
+  @override
+  State<quizpage> createState() => _quizpageState();
+}
+
+class _quizpageState extends State<quizpage> {
+  @override
+  Widget build(BuildContext context) {
+    return  Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Expanded(
+          flex: 5,
+          child: Padding(
+              padding: EdgeInsets.all(10.0),
+            child: Text(
+              'This is where question text will go',
+          textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 25.0,
+                color: Colors.white
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+              padding: EdgeInsets.all(15.0),
+          child: TextButton(
+              style: TextButton.styleFrom(
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.zero ),
+                backgroundColor : Colors.green ,
+              ),
+              onPressed: ()
+              {
+            },
+            child: Text(
+              'True',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20.0
+              ),
+            ),
+          
+          ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.zero ),
+                backgroundColor : Colors.red ,
+              ),
+              onPressed: ()
+              {
+              },
+              child: Text(
+                'False',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0
+                ),
+              ),
+
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+
